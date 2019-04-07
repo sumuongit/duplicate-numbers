daabf1644ee66023fe1b5a42e61f489ac82dcc54
Salahuddin Ahmed <sumu2007@gmail.com>
2019-04-07 13:04:29 +0600

    Duplicate numbers are now shown as comma separated

--- DuplicateNumber/Program.cs
+++ DuplicateNumber/Program.cs
@@ -9,8 +9,7 @@ namespace DuplicateNumber
     public class Duplicate
     {
         int[] numArray = new int[] { 11, 15, 3, 18, 30, 22, 37, 7, 11, 8, 22, 2, 50, 41, 15, 30, 16, 27, 19, 7 };
-        int temp;
-        int count;
+        int temp, count;
         List<int> duplicateNumberList = new List<int>();
 
         public List<int> FindDuplicateNumber()
@@ -44,11 +43,18 @@ namespace DuplicateNumber
     {
         static void Main(string[] args)
         {
-            Duplicate dp = new Duplicate();           
-            foreach (int item in dp.FindDuplicateNumber())
-            {
-                Console.Write("{0} ", item);
-            }
+            Duplicate dp = new Duplicate();
+            string numWithCommas = string.Join(",", dp.FindDuplicateNumber());
+
+            //DISPLAY NUMBERS WITH COMMAS NOT USING FOREACH LOOP
+            Console.Write("{0} ", numWithCommas);
+
+            //DISPLAY NUMBERS WITHOUT COMMAS USING FOREACH LOOP
+            //foreach (int item in dp.FindDuplicateNumber())
+            //{                
+            //    Console.Write("{0} ", item);
+            //}
+
             Console.ReadLine();
         }
     }
