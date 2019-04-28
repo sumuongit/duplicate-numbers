07d3a6d7c86e850185cffa71074a9d511be069d7
sumonongit <sumu2007@gmail.com>
2019-04-28 23:44:49 +0600

    Code optimization has been done

--- DuplicateNumber/Program.cs
+++ DuplicateNumber/Program.cs
@@ -8,32 +8,38 @@ namespace DuplicateNumber
     {
         int[] numArray = new int[] { 11, 15, 3, 18, 30, 22, 37, 7, 11, 8, 22, 2, 50, 41, 15, 30, 16, 27, 19, 7 };
         int temp, count;
-        List<int> duplicateNumberList = new List<int>();
+        List<int> duplicateNumberList = new List<int>();        
 
         public List<int> FindDuplicateNumber()
         {
+            //COMPARE EVERY ITEM OF THE GIVEN ARRAY WITH ALL THE ITEMS OF THE ARRAY
             for (int i = 0; i < numArray.Count(); i++)
             {
                 temp = numArray[i];
                 count = 0;
-               
-                for (int j = 0; j < numArray.Count(); j++)
+
+                //ALREADY DUPLICATED NUMBER SHOULD NOT BE COMPARED AGAIN WITH ALL THE ITEMS OF THE GIVEN ARRAY
+                if (!duplicateNumberList.Contains(temp))
                 {
-                    if(temp == numArray[j])
+                    for (int j = 0; j < numArray.Count(); j++)
                     {
-                        count = count + 1;
+                        if (temp == numArray[j])
+                        {
+                            count = count + 1;
+                        }
+                    }
+
+                    //MORE THAN ONE TIME OF OCCURRENCE OF A NUMBER MEANS IT IS A DUPLICATE NUMBER
+                    if (count > 1)
+                    {
+                        if (!duplicateNumberList.Contains(temp))
+                        {
+                            duplicateNumberList.Add(temp); 
+                        }
                     }
                 }
-
-                if(count == 2)
-                {
-                    if (!duplicateNumberList.Contains(temp))
-                    {
-                        duplicateNumberList.Add(temp);
-                    }
-                }              
             }
-
+            
             return duplicateNumberList;
         }
     }
@@ -44,7 +50,7 @@ namespace DuplicateNumber
             Duplicate dp = new Duplicate();
             string numWithCommas = string.Join(", ", dp.FindDuplicateNumber());
             Console.Write("Duplicate numbers are as follows: ");
-            Console.Write("{0} ", numWithCommas);
+            Console.Write("{0} ", numWithCommas);           
             Console.ReadLine();
         }
     }
